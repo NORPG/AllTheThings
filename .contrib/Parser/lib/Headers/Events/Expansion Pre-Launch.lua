54a3dfbed7e74ebd094ed4a4a150f19f889d1300
Chun-Chi Hung <b10102229@gmail.com>
2025-01-24 22:24:03 +0800

    [DB] Update header: Expansion Pre-Launch. Replace custom header by new style.

---
+++ .contrib/Parser/lib/Headers/Events/Expansion Pre-Launch.lua
@@ -0,0 +1,12 @@
+EXPANSION_PRELAUNCH = createHeader({
+	readable = "Expansion Pre-Launch",
+	constant = "EXPANSION_PRELAUNCH",
+	icon = 134289,
+	text = {
+		en = "Expansion Pre-Launch",
+		fr = "Pré-Lancement de l’Extension",
+		ru = "Препатч",
+		cn = "资料片前夕",
+		tw = "資料片前夕",
+	},
+});
