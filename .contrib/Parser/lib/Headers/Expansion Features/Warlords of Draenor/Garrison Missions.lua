595910d719ac3bfd485327824681dbd53c1646c0
Chun-Chi Hung <b10102229@gmail.com>
2025-01-25 01:15:23 +0800

    [DB] Update header: Garrison Missions. Replace custom header by new style.

---
+++ .contrib/Parser/lib/Headers/Expansion Features/Warlords of Draenor/Garrison Missions.lua
@@ -0,0 +1,8 @@
+GARRISON_MISSIONS = createHeader({
+    readable = "Garrison Missions",
+    constant = "GARRISON_MISSIONS",
+    icon = 1035062,
+    text = {
+        en = [[~GARRISON_MISSIONS]],
+    },
+});
