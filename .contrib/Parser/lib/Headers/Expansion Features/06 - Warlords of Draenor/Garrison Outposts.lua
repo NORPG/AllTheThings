39d98c076d61df5d9d292d750d535e2870cdb935
Chun-Chi Hung <b10102229@gmail.com>
2025-01-30 23:03:23 +0800

    [DB] Retail: Add Garrison Outposts.

---
+++ .contrib/Parser/lib/Headers/Expansion Features/06 - Warlords of Draenor/Garrison Outposts.lua
@@ -0,0 +1,55 @@
+SPARRING_ARENA = createHeader({
+	readable = "Sparring Arena",
+	text = {
+		en = "Sparring Arena",
+	},
+});
+
+LUMBER_YARD = createHeader({
+	readable = "Lumber Yard",
+	text = {
+		en = "Lumber Yard",
+	},
+});
+
+ARCANE_SANCTUM = createHeader({
+	readable = "Arcane Sanctum",
+	text = {
+		en = "Arcane Sanctum",
+	},
+});
+
+ARSENAL = createHeader({
+	readable = "Arsenal",
+	text = {
+		en = "Arsenal",
+	},
+});
+
+TANKWORKS = createHeader({
+	readable = "Tankworks",
+	text = {
+		en = "Tankworks",
+	},
+});
+
+CORRAL = createHeader({
+	readable = "Corral",
+	text = {
+		en = "Corral",
+	},
+});
+
+SMUGGLING_RUN = createHeader({
+	readable = "Smuggling Run",
+	text = {
+		en = "Smuggling Run",
+	},
+});
+
+BREWERY = createHeader({
+	readable = "Brewery",
+	text = {
+		en = "Brewery",
+	},
+});
