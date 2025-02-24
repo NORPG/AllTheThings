480a4bd2e96a546b2fa9f2f7b3bce8315e9b3318
Chun-Chi Hung <b10102229@gmail.com>
2025-02-25 05:23:31 +0800

    [DB] Update header: Skoldus Hall.

--- locales/en.lua
+++ locales/en.lua
@@ -39,7 +39,6 @@ for key, value in pairs({
 	-- Mists of Pandaria PvP Seasons
 	-- Warlords of Dreanor PvP Seasons
 	-- SL Headers
-		[-982] = 2178518,																	-- Skoldus Hall
 		[-983] = 1392920,																	-- Fracture Chambers
 		[-984] = 1392929,																	-- The Soulforges
 		[-985] = 1392911,																	-- Coldheart Interstitia
@@ -72,7 +71,6 @@ for key, value in pairs({
 	-- Warlords of Dreanor PvP Seasons
 	-- Shadowlands Header
 		-- SL Torghast
-		[-982] = select(2, GetAchievementInfo(14463)),							-- Skoldus Hall
 		[-983] = select(2, GetAchievementInfo(14473)),							-- Fracture Chambers
 		[-984] = select(2, GetAchievementInfo(14478)),							-- The Soulforges
 		[-985] = select(2, GetAchievementInfo(14483)),							-- Coldheart Interstitia
