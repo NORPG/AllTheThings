fcf75db2a0947f19a009b7ed09146fea2b933b36
Dmitry Volodin <mr.molkree@gmail.com>
2026-03-23 02:20:45 +0400

    Hardcoded two more localization strings that are not present in classic flavors

--- .contrib/Parser/lib/Strings/Settings/Features - Commands/ILLUSION.lua
+++ .contrib/Parser/lib/Strings/Settings/Features - Commands/ILLUSION.lua
@@ -3,6 +3,17 @@ createLocalizationString({
 	constant = "ILLUSION",
 	export = true,
 	text = {
-		en = [[~PERKS_VENDOR_CATEGORY_ILLUSION]],
+		-- en = [[~PERKS_VENDOR_CATEGORY_ILLUSION]],	-- missing in classic flavors
+		en = "Illusion",
+		de = "Illusion",
+		es = "Ilusión",
+		mx = "Ilusión",
+		fr = "Illusion",
+		it = "Illusione",
+		pt = "Ilusão",
+		ru = "Чары оружия",
+		ko = "환영",
+		cn = "幻影",
+		tw = "幻影",
 	},
 })
--- .contrib/Parser/lib/Strings/Settings/Features - Commands/RUNECARVING_POWER.lua
+++ .contrib/Parser/lib/Strings/Settings/Features - Commands/RUNECARVING_POWER.lua
@@ -3,6 +3,17 @@ createLocalizationString({
 	constant = "RUNECARVING_POWER",
 	export = true,
 	text = {
-		en = [[~RUNEFORGE_LEGENDARY_POWER_LABEL]],
+		-- en = [[~RUNEFORGE_LEGENDARY_POWER_LABEL]],	-- missing in classic flavors
+		en = "Runecarving Power",
+		de = "Runenschnitzmacht",
+		es = "Poder de talla de runas",
+		mx = "Poder de Tallado rúnico",
+		fr = "Pouvoir de la gravure runique",
+		it = "Abilità dell'Incisione Runica",
+		pt = "Poder de Runogravura",
+		ru = "Вырезание рун",
+		ko = "룬조각 능력",
+		cn = "符文铭刻之力",
+		tw = "符文雕刻力量",
 	},
 })
