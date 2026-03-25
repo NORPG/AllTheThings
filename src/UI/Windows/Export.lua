fcb8f9fa2dd12da27049fb22faef0b9e2c5fe253
ImUnicke <ImUnicke+github@gmail.com>
2026-03-25 12:44:57 -0600

    [Export] Improved names of defined Export styles [Export] More improvements to Export window functionality

--- src/UI/Windows/Export.lua
+++ src/UI/Windows/Export.lua
@@ -4,17 +4,37 @@ local L = app.L;
 
 local ExportStyle
 local function UpdateExportStyle(row, button, styleID, active)
-	ExportStyle = styleID
+	if active then
+		ExportStyle = styleID
+	else
+		ExportStyle = nil
+	end
+	-- clear other checks
+	local styleGroup = row.ref and row.ref.parent
+	if not styleGroup then return end
+
+	local g = styleGroup.g
+	for i=1,#g do
+		if g[i].toggleID ~= styleID then
+			g[i].saved = nil
+		end
+	end
 end
-local function DoExport(t)
+local function DoExport(row, button)
 	if not ExportStyle then
 		app.print("Please select a Style to Export")
 		return
 	end
-	local window = app:GetWindow(t.text)
+	local window = app:GetWindow(row.ref.Suffix)
 	app:ExportStylizedData(window, ExportStyle)
-	app.print("Exported",window.Suffix,"as",ExportStyle,"style!")
+	app.print("Exported",window.Suffix,"in",ExportStyle,"style!")
 end
+
+-- Excluded windows
+local ExcludedWindows = {
+	Export = 1,
+	Prime = 1,
+}
 
 -- Window
 app:CreateWindow("Export", {
@@ -25,48 +45,57 @@ app:CreateWindow("Export", {
 	OnInit = function(self, handlers)
 		local styleGroup = app.CreateRawText("Style", {
 			icon = app.asset("Category_TradingPost"),
-			visible = true,
 			expanded = true,
 			back = 0.5,
 			SortType = "Global",
 			g = {},
 		})
 		-- self.StyleGroup = styleGroup
+		local styleGroupg = styleGroup.g
 		for key in pairs(app.DataStyleExporters) do
-			tinsert(styleGroup.g, app.CreateToggle(key, {
+			styleGroupg[#styleGroupg + 1] = app.CreateToggle(key, {
 				name = key,
 				parent = styleGroup,
 				visible = true,
 				OnUpdate = app.AlwaysShowUpdate,
 				OnClickHandler = UpdateExportStyle
-			}))
+			})
 		end
 
 		local windowsGroup = app.CreateRawText("Windows", {
 			icon = app.asset("Category_WorldDrops"),
-			visible = true,
 			expanded = true,
 			back = 0.5,
+			SortType = "Global",
 			g = {},
-			OnUpdate = function(t)
-				if not t.expanded then return end
-
-				local rows = t.g
-				wipe(rows)
-				for suffix,window in pairs(app.Windows) do
-					-- TODO: save a hash table of existing window-row links and just skip re-adding
-					-- TODO: maybe wrap the window header data instead
-					tinsert(rows, app.CreateRawText(window.Suffix, {
-						OnUpdate = app.AlwaysShowUpdate,
-						OnClick = DoExport,
-						parent = t,
-						back = 0.2,
-					}))
-				end
-				t.SortType = "Global"
-				return true
-			end,
 		})
+		local rows = windowsGroup.g
+		for suffix,window in pairs(app.Windows) do
+			-- TODO: maybe wrap the window header data instead
+			if not ExcludedWindows[suffix] then
+				rows[#rows + 1] = app.CreateRawText(suffix, {
+					Suffix = suffix,
+					OnUpdate = app.AlwaysShowUpdate,
+					OnClick = DoExport,
+					parent = windowsGroup,
+					back = 0.2,
+				})
+			end
+		end
+		app.AddEventHandler("OnWindowCreated", function(window, suffix)
+			if not ExcludedWindows[suffix] then
+				local newWindowGroup = app.CreateRawText(suffix, {
+					Suffix = suffix,
+					OnUpdate = app.AlwaysShowUpdate,
+					OnClick = DoExport,
+					parent = windowsGroup,
+					back = 0.2,
+				})
+				rows[#rows + 1] = newWindowGroup
+				windowsGroup.SortType = "Global"
+				app.DirectGroupUpdate(windowsGroup)
+			end
+		end)
 
 		self:SetData(app.CreateRawText("Export", {
 			-- icon = app.asset("Interface_Vendor"),
