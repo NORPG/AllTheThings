f5c9e918187810abc84910a477dc0933d4f58dd5
Dmitry Volodin <mr.molkree@gmail.com>
2026-03-29 19:33:52 +0400

    Marked some promo stuff as removed

--- .contrib/Parser/DATAS/12 - Promotions/Discord.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Discord.lua
@@ -40,7 +40,7 @@ root(ROOTS.Promotions, n(DISCORD_PROMOTION, {
 		}),
 		i(264366, {	-- Razeshi C. (PET!)
 			["description"] = "At the top of your Direct messages tab on Discord, click the Quests tab to start the Quest for the Reward.\n\nPlay the game for 15 minutes with Discord running.\n\nUpon completion of the quest, you'll be given a code to redeem - head to the Battle.net launcher, click your profile in the top right, and choose 'Redeem Code.'\n\nPromotion is available from March 2nd, 2026 until March 16th, 2026.",
-			["timeline"] = { "added 12.0.1.66198", "removed 12.0.1.99999" },
+			["timeline"] = { "added 12.0.1.66198", "removed 12.0.1.66384" },
 		}),
 	},
 }));
