b080eff8abe4179018c8bba1218bf78780e1f827
Darkal <michalbert@seznam.cz>
2026-03-31 17:54:00 +0200

    This should be commited first next time, unlucky.

---
+++ .contrib/Parser/lib/Headers/Profession/Profession Nodes.lua
@@ -0,0 +1,17 @@
+PROFESSION_NODES_HEADER = createHeader({
+	readable = "Profession Nodes",
+	icon = 4622272,
+	text = {
+		en = "Profession Nodes",
+		-- de = "",
+		-- es = "",
+		-- mx = "",
+		-- fr = "",
+		-- it = "",
+		-- ko = "",
+		-- pt = "",
+		-- ru = "",
+		-- cn = "",
+		-- tw = "",
+	},
+});
