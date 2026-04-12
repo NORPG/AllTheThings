eaa6baa4abba45517b002039cbccd030a02897b0
Exodius <Exodius@users.noreply.github.com>
2026-04-12 17:15:37 +0200

    Silvermoon City/Pets: Add a set of coordinates for 'Silvermoon Broom'

--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/1 - Silvermoon City/Battle Pets & Wild Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Quel'Thalas/1 - Silvermoon City/Battle Pets & Wild Pets.lua
@@ -6,7 +6,13 @@ root(ROOTS.Zones, m(MAP.MIDNIGHT.QUELTHALAS, {
 		filter(BATTLE_PETS, {
 			i(258660, {	-- Silvermoon Broom (PET!)
 				["crs"] = { 254885 },	-- Silvermoon Broom
-				["coords"] = {
+				["coords"] = {	-- Do not separate currently added coords. These are per spotted pet that wanders between these. If you find a new one, just add below.
+					-- Falconwing Square, swipes the floor between these 4 points around the structure
+					{ 28.7, 75.6, MAP.MIDNIGHT.SILVERMOON_CITY },
+					{ 29.0, 81.4, MAP.MIDNIGHT.SILVERMOON_CITY },
+					{ 31.8, 81.2, MAP.MIDNIGHT.SILVERMOON_CITY },
+					{ 32.4, 75.4, MAP.MIDNIGHT.SILVERMOON_CITY },
+					--
 				},
 			}),
 		}),
