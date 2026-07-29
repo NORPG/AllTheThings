aa4fb073f5c7e5ca668425100bd21df89a14d759
ImUnicke <ImUnicke+github@gmail.com>
2026-07-29 13:51:39 -0600

    [Logic] Simpler secret value check in NPCNameFromID

--- src/Classes/NPC.lua
+++ src/Classes/NPC.lua
@@ -67,7 +67,7 @@ if C_TooltipInfo_GetHyperlink then
 					end
 				end
 				-- return a secret name but don't cache it for re-use
-				if app.WOWAPI.issecretvalue(title) then
+				if issecretvalue(title) then
 					-- app.PrintDebug("returning secret name", title)
 					return title
 				end
