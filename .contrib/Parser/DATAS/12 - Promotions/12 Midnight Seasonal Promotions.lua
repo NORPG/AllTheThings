fbe7fb183a67cb3093d3db4eeea9ccd82b5c45c7
ImUnicke <ImUnicke+github@gmail.com>
2026-07-31 15:07:31 -0600

    [DB] Additional note on Craft Your World achievement

--- .contrib/Parser/DATAS/12 - Promotions/12 Midnight Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/12 Midnight Seasonal Promotions.lua
@@ -152,7 +152,7 @@ root(ROOTS.Promotions, {
 				["timeline"] = { "added 12.0.1.65899", "removed 12.1.0.99999" },	-- TODO: Timeline out. Available through September 30, 2026.
 			}),
 			ach(62400, {	-- Craft Your World
-				["description"] = "Open Options\n\nGo to Gameplay -> Social\n\nCheck Connect to Pinterest\n\nSign in through the in-game browser and authorize the connection.",
+				["description"] = "Open Options\n\nGo to Gameplay -> Social\n\nCheck Connect to Pinterest\n\nSign in through the in-game browser and authorize the connection.\n\nNote: If any sort of Parental Controls have been set up on your account, this will not be visible in the Options menu unless they are fully removed via Battle Net support ticket.",
 				["timeline"] = { "added 12.0.1.66017" },
 				["groups"] = { i(268695) },	-- Pin-o-Matic Camera (TOY!)
 			}),
