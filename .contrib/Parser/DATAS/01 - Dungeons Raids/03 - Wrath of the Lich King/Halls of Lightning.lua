ca49a308656da0d4a3bd927804025b21e0d8be84
milotic <al0305@hotmail.com>
2026-07-31 20:15:23 -0600

    Fix parser

--- .contrib/Parser/DATAS/01 - Dungeons Raids/03 - Wrath of the Lich King/Halls of Lightning.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/03 - Wrath of the Lich King/Halls of Lightning.lua
@@ -5,7 +5,6 @@
 root(ROOTS.Instances, expansion(EXPANSION.WRATH, applyclassicphase(WRATH_PHASE_ONE, bubbleDownSelf({ ["timeline"] = { ADDED_3_0_2 } }, {
 	inst(275, {	-- Halls of Lightning
 		["lore"] = "Upon departing Azeroth, the titans entrusted loyal protectors with safeguarding Ulduar, an enigmatic city nestled in the mountains of the Storm Peaks. Designated supreme among his brethren was the guardian known as Loken, but with the power of the titan complex in his hands, he turned to darkness and plunged the region into chaos.",
-		-- #endif
 		["mapID"] = HALLS_OF_LIGHTNING,
 		["coord"] = { 38.0, 14.7, THE_STORM_PEAKS },
 		["maps"] = { HALLS_OF_LIGHTNING_FLOOR2 },
