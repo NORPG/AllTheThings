d9b6a06c5da5be746a3c918b1ee555e75a4c5e78
ImUnicke <ImUnicke+github@gmail.com>
2026-08-13 09:11:48 -0600

    [DB] Pepe object name

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_639222305354592480.lua
@@ -0,0 +1,20 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[657746] = {
+		readable = "Pepe",
+		model = 7959391,
+		text = {
+			en = "Pepe",
+			es = "[Pepe]",
+			mx = "[Pepe]",
+			de = "[Pepe]",
+			fr = "[Pepe]",
+			it = "[Pepe]",
+			pt = "[Pepe]",
+			ru = "[Pepe]",
+			ko = "[Pepe]",
+			cn = "[Pepe]",
+			tw = "[Pepe]",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
