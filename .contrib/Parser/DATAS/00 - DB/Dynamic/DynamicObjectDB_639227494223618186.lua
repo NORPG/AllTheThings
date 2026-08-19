c3b9d1068046e2f67e40f7c16fc0f96989175715
ImUnicke <ImUnicke+github@gmail.com>
2026-08-19 11:10:17 -0600

    [DB] Some Object harvests while testing

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_639227494223618186.lua
@@ -0,0 +1,113 @@
+﻿
+local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[387696] = {
+		text = {
+			es = "Bolsa de monedas de Warcraft Rumble",
+			mx = "Bolsa de monedas Rumble",
+			de = "Rumble-Münzenbeutel",
+			fr = "Sac de pièces Rumble",
+			it = "Borsa di Monete di Rumble",
+			pt = "Bolsa de Moedas de Rumble",
+			ru = "Мешочек с монетами Rumble",
+			ko = "럼블 코인 가방",
+			cn = "魔兽游戏币袋",
+			tw = "兵團零錢袋",
+		},
+	},
+	[387719] = {
+		text = {
+			es = "Caja de brillos de Warcraft Rumble",
+			mx = "Bolsa de láminas de Rumble",
+			de = "Rumble-Folientasche",
+			fr = "Sac de Rumble métallisés",
+			it = "Borsa di Fogli di Rumble",
+			pt = "Bolsa de Metalizados de Rumble",
+			ru = "Мешочек с оберткой Rumble",
+			ko = "럼블 박지 가방",
+			cn = "魔兽游戏箔纸袋",
+			tw = "兵團錫箔包裝袋",
+		},
+	},
+	[403541] = {
+		text = {
+			es = "Varita de polimorfia en masa",
+			mx = "Varita de polimorfia en masa",
+			de = "Zauberstab der Massenverwandlung",
+			fr = "Baguette de Métamorphose de masse",
+			it = "Bacchetta della Metamorfosi di Massa",
+			ru = "Жезл массового превращения",
+		},
+	},
+	[404319] = {
+		text = {
+			es = "Fragmento perdido en el tiempo",
+			mx = "Fragmento de tiempo perdido",
+			de = "Zeitverlorenes Fragment",
+			fr = "Fragment perdu dans le temps",
+			it = "Frammento Perduto nel Tempo",
+			pt = "Fragmento Perdido no Tempo",
+			ru = "Затерянный во времени фрагмент",
+			ko = "잃어버린 시간의 파편",
+		},
+	},
+	[405010] = {
+		text = {
+			es = "Barril de la cuenta",
+			mx = "Barril de la cuenta del bar",
+			de = "Schankfass",
+			fr = "Tonneau d’ardoise de bar",
+			it = "Barilotto",
+			pt = "Barril na Conta",
+			ru = "Полная бочка",
+			ko = "술집 외상 통",
+			cn = "熟客酒桶",
+			tw = "酒吧收帳桶",
+		},
+	},
+	[405068] = {
+		text = {
+			es = "Grimorio olvidado",
+			mx = "Grimorio olvidado",
+			de = "Vergessenes Grimoire",
+			fr = "Grimoire oublié",
+			it = "Rito Dimenticato",
+			pt = "Grimório Esquecido",
+			ru = "Забытый гримуар",
+			ko = "잊힌 고서",
+			cn = "被遗忘的魔典",
+			tw = "被遺忘的魔典",
+		},
+	},
+	[405286] = {
+		text = {
+			es = "Página rota",
+			de = "Ausgerissene Seite",
+			fr = "Page déchirée",
+			it = "Pagina Strappata",
+			pt = "Página Rasgada",
+			ko = "찢어진 책장",
+		},
+	},
+	[405489] = {
+		text = {
+			es = "Armario de conservacionista",
+			mx = "Armario de conservacionista",
+			de = "Kiste der Bewahrerin",
+			fr = "Coffre de la préservatrice",
+			it = "Armadietto della Preservatrice",
+			pt = "Armário da Conservacionista",
+			ru = "Сундук реставратора",
+			ko = "보존가의 보관함",
+			cn = "捍卫者的柜子",
+			tw = "保護學家的儲物箱",
+		},
+	},
+	[405510] = {
+		text = {
+			es = "Palanca antigua",
+			mx = "Palanca antigua",
+			fr = "Levier ancien",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
