0cdd427f8551af6ce4221d64f07dd504ba6ecc24
Chun-Chi Hung <b10102229@gmail.com>
2026-09-16 05:52:43 +0800

    [DB] Forever: add Ruins of Lordaeron.

---
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Forever/Ruins of Lordaeron.lua
@@ -0,0 +1,73 @@
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+
+-- #if FOREVER
+--[[
+root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
+applyclassicphase(FOREVER_PHASE_ONE, inst(RUINS_OF_LORDAERON, bubbleDownSelf({
+	["timeline"] = { ADDED_1_16_0 },
+}, {
+	["lore"] = "",
+	["zone-text-areaID"] = ,
+	["coord"] = { 50.0, 50.0, TIRISFAL_GLADES },
+	["lvl"] = 15,
+	["groups"] = {
+		n(, {
+			i(, {
+				["classes"] = { },
+			}),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+			i(),
+		}),
+		n(, {
+			["timeline"] = { },
+			["modelScale"] = ,
+			["groups"] = {
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+				i(, {
+					["timeline"] = { },
+				}),
+			},
+		}),
+	},
+}))),
+}));
+]]
+-- #endif
