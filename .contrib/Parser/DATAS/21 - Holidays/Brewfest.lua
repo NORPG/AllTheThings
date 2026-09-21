95f9f87986208ab3b19fba697f79c15eabdcf2ea
ImUnicke <ImUnicke+github@gmail.com>
2026-09-20 22:28:28 -0600

    [DB] 94054 flags account wide on first Coren kill

--- .contrib/Parser/DATAS/21 - Holidays/Brewfest.lua
+++ .contrib/Parser/DATAS/21 - Holidays/Brewfest.lua
@@ -3737,7 +3737,7 @@ root(ROOTS.HiddenQuestTriggers, {
 	expansion(EXPANSION.MID, bubbleDownSelf({ ["timeline"] = { ADDED_12_1_0 } }, {
 		applyevent(EVENTS.BREWFEST, n(BREWFEST_HEADER, {
 			q(94053),	-- Killed Coren Direbrew (Not Accountwide)
-			--q(94054),	--
+			q(94054),	-- Killed Coren Direbrew (Accountwide)
 		})),
 	})),
 });
