af320a10fedc455d054502d6c3ef6833a1d95822
ImUnicke <ImUnicke+github@gmail.com>
2026-09-22 20:42:11 -0600

    [DB] Mythic Venomweaver Vexhiss HQT

--- .contrib/Parser/DATAS/01 - Dungeons Raids/12 - Midnight/5 - The Venomous Abyss.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/12 - Midnight/5 - The Venomous Abyss.lua
@@ -660,6 +660,7 @@ root(ROOTS.HiddenQuestTriggers, expansion(EXPANSION.MID, {
 			hqt(98077, name(HEADERS.NPC, 261492)),	-- Gorefang the Reaver
 			hqt(98089, name(HEADERS.NPC, 266538)),	-- Shriekcoil
 			hqt(98073, name(HEADERS.NPC, 261503)),	-- Frightmaw
+			hqt(98085, name(HEADERS.NPC, 263594)),	-- Venomweaver Vexhiss
 		},
 	}),
 }))
